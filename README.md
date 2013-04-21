61146cb6b7241b3f07e1ce2cf8b6209ac218e84e
Vikash Kumar <vikashskumar@gmail.com>
2013-04-21 19:13:50 +0530

    initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Ruby on Rails Tutorial: demo app
+
+This is the first application for
+[*Ruby on Rails Tutorial: Learn Rails by Example*](http://railstutorial.org/)
+This is done by [Vikash Kumar](http://www.vikashkumar.com/).
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = 'f85ba397a0b8d65d0a1673f52d6da55d3eb8b0a903d41065b9b46427e3fd7fc90340cee81a364d728d657e12d9fe991b36818ccd47968bc65bf998f962a0e3ec'
